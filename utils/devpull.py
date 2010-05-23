448e721d0bc4a67b4304551e39f2f5076e162948
octaflop <octaflop@gmail.com>
2010-05-23 13:19:14 -0700

    initial commit and project outline

---
+++ utils/devpull.py
@@ -0,0 +1,17 @@
+"""gitdep = {
+    '
+    }
+hgdep = {
+    'flask':'<hg1>',
+    'sqlalchemy':'<hg2>',
+    '':'',
+    }
+
+def hg_dep_dl():
+
+def git_dep_dl():
+
+if __name__ == "__main__":
+    
+    TODO
+    """
