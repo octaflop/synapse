26fa0e70d5f3eafe770fdac24dfed616f4745a6d
octaflop@gmail.com <octaflop@gmail.com>
2010-05-23 22:13:53 -0700

    a lot of additions. login just needs to work

--- settings.py
+++ settings.py
@@ -2,3 +2,7 @@ from passwd import *
 DEV = True
 SECRET_KEY = 'vba937ei38mq2'
 UPLOAD_FOLDER = '/tmp/uploads/'
+g = {}
+g['user'] = None
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
