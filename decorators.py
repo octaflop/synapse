ef00c7ecc44b767c5b5545fce606f1751b71d9a9
foenix <foenickz@gmail.com>
2010-05-23 22:13:53 -0700

    a lot of additions. login just needs to work

---
+++ decorators.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- encoding:utf8 -*-
+
+from functools import wraps
+from flask import g, request, redirect, url_for, render_template, session
+
+def cached(timeout=5 * 60, key='view/%s'):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            cache_key = key % request.path
+            rv = cache.get(cache_key)
+            if rv is not None:
+                return rv
+            rv = f(*args, **kwargs)
+            cache.set(cache_key, rv, timeout=timeout)
+            return rv
+        return decorated_function
+    return decorator
+
+def template(template=None):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            template_name = template
+            if template_name is None:
+                template_name = request.endpoint \
+                    .replace('.', '/') + '.html'
+            ctx = f(*args, **kwargs)
+            if ctx is None:
+                ctx = {}
+            return render_template(template_name, **ctx)
+        return decorated_function
+    return decorator
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session['user'] is None:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
