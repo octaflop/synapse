9642449f89fafa50276a9579b5a09584529b7927
Faris <octaflop@gmail.com>
2010-05-27 11:30:16 -0600

    Current production branch

--- models.py
+++ models.py
@@ -1,7 +1,4 @@
 import redis
-from settings import REDIS_SERVER, REDIS_PASSWD, DEV
+#from settings import REDIS_SERVER, REDIS_PASSWD, DEV
 
-if DEV:
-        R = redis.Redis()
-elif not DEV:
-	R = redis.Redis(host=REDIS_SERVER, password=REDIS_PASSWD)
+R = redis.Redis()
