cc82cb3cacd07af916be7cf78db1d80bf2e36e2d
Faris <octaflop@gmail.com>
2010-08-10 11:17:51 -0700

    New iteration of some blogging software

--- models.py
+++ models.py
@@ -1,330 +1,46 @@
-import redis
-import hashlib
+# -*- encoding: utf-8 -*-
+from strings import *
 import uuid as ui
 import datetime
 
-from strings import *
-#from settings import REDIS_SERVER, REDIS_PASSWD, DEV, SALT
-SALT = "FAkeSa8r3y2qwi" # still devving it up, so this is here.
+from mongoengine import *
+connect('aliendog')
 
-#if DEV:
-R = redis.Redis() #This is where we link up to good ol' redis
-#elif not DEV:
-#	R = redis.Redis(host=REDIS_SERVER, password=REDIS_PASSWD)
+SALT = "FAkeSa8r3y2qwi"
+EMAILREG =\
+"/^([\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+\.)*[\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+@((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)$/i"
 
-class Thing():
-    def __init__(self, kind=None):
-        if kind is None:
-            self.kind = 'thing'
-        elif type(kind) is str:
-            self.kind = kind
-        else:
-            raise TypeError("got " + kind + " as type:" + type(kind) + ". Need\
-                    str")
-        self.creation = str(datetime.datetime.now())
-        self.name = u""
-        self.uuid = str(ui.uuid1())
-        self.attrs = {}
+class Site(Document):
+    title = StringField(required=True)
+    domain = StringField(required=True)
 
-    def _exists(self, kind, uuid):
-        if not R.sismember("%s" % kind, uuid):
-            print "not in database"
-            return False
-        elif R.sismember("%s" % kind, uuid):
-            return True
+class User(Document):
+    email = StringField(required=True, regex=EMAILREG)
+    username = StringField(required=True, unique=True)
+    first_name = StringField(max_length=50)
+    last_name = StringField(max_length=50)
+    hashedpassword = StringField()
+    date_created = DateTimeField()
 
-    def _checkid(self, kind, uuid):
-        if self.kind is None:
-            if kind is None:
-                self.kind = 'thing'
-            elif type(kind) is str:
-                self.kind = kind
-            elif type(kind) is unicode:
-                self.kind = kind
-            else:
-                print "input a proper kind"
-                return False
-        elif type(self.kind) is str:
-            kind = self.kind
-        elif type(self.kind) is unicode:
-            kind = self.kind
-        if self.uuid is None:
-            if uuid is None:
-                print "Error. Instantiate a uuid or supply one."
-                return False
-            elif isinstance(uuid, str):
-                self.uuid = uuid
-                return True
-            elif isinstance(uuid, unicode):
-                self.uuid = uuid
-                return True
-        elif isinstance(self.uuid, str):
-            uuid = self.uuid
-            return True
-        elif isinstance(self.uuid, unicode):
-            uuid = self.uuid
-            return True
-        else:
-            print "Some sort of error occurred"
-            return False
+class Comment(EmbeddedDocument):
+    content = StringField()
+    name = StringField(max_length=120)
+    date_created = DateTimeField()
 
-    def get_tid(self, kind=None, uuid=None):
-        assert self._checkid(kind, uuid)
-        self.tid = R.get("%s:%s:tid" % (self.kind, self.uuid))
-        return self.tid
+class Post(Document):
+    title = StringField(max_length=120)
+    author = ReferenceField(User)
+    slug = StringField()
+    tags = ListField(StringField(max_length=45))
+    date_created = DateTimeField()
 
-    def get(self, kind=None, uuid=None):
-        """
-        if kind is None and uuid is None:
-            self.kind = kind
-            self.uuid = uuid
-        else:
-            kind = self.kind
-            uuid = self.uuid
-            """
-        assert self._checkid(self.kind, self.uuid)
-        assert self._exists(self.kind, self.uuid)
-        self.tid = self.get_tid(self.kind, self.uuid)
-        redkey = "%s:%s" % (self.kind, self.tid)
-        ret = R.hgetall(redkey)
-        self.attrs = ret
-        return ret
+class TextPost(Post):
+    author = ReferenceField(User)
+    content = StringField()
 
-    def post(self, attrs=None):
-        try:
-            R.sadd("%s" % self.kind, self.uuid)
-            print "woot!"
-        except:
-            print "already in database"
-            return False
-        print R.sismember("%s" % self.kind, self.uuid)
-        self.tid = R.zincrby("global:%s:tid" % self.kind, 1) # tid = "thing id"
-        self.tid = int(self.tid)
-        R.set("%s:%s:tid" % (self.kind, self.uuid), self.tid)
-        try:
-            self.slug = slugfy(self.name)
-        except:
-            self.slug = slugfy('anon')
-        self.uurl = add_uurl(self.kind, self.slug, self.tid, self.uuid)
-        redkey = "%s:%s" % (self.kind, self.tid)
-        if attrs and self.attrs is None:
-            self.attrs = {}
-        elif isinstance(attrs, dict):
-            self.attrs = attrs
-        elif isinstance(self.attrs, dict):
-            attrs = self.attrs
-        else:
-            print "Attrs are invalid type"
-            return False
-        self.attrs['creation'] = self.creation
-        self.attrs['uuid'] = self.uuid
-        R.hmset(redkey, self.attrs)
-        for attr in self.attrs.keys():
-            key = redkey + ":" + attr
-            R.set(key, self.attrs[attr])
-        return True
+class ImagePost(Post):
+    image_path = StringField()
 
-    def put(self, attrs, kind=None, uuid=None):
-        assert isinstance(attrs, dict)
-        assert self._checkid(kind, uuid)
-        assert self._exists(self.kind, self.uuid)
-        self.tid = self.get_tid(self.kind, self.uuid)
-        self.attrs = attrs
-        redkey = "%s:%s" % (self.kind, self.tid)
-        ret = ""
-        ret += str(R.hmset(redkey, self.attrs))
-        for attr in attrs.keys():
-            key = redkey + ":" + attr
-            R.set(key, attrs[attr])
-            ret += str(R.hset(redkey, attr, attrs[attr]))
-        return ret
-
-    def delete(self, kind=None, uuid=None):
-        assert self._checkid(kind, uuid)
-        assert self._exists(self.kind, self.uuid)
-        self.tid = self.get_tid(self.kind, self.uuid)
-        redkey = "%s:%s" % (self.kind, self.tid)
-        R.srem("%s" % self.kind, self.uuid)
-        R.delete(redkey)
-        return True
-
-    def _url(self):
-        uurl = uuid2uurl(self.uuid)
-        return uurl
-
-    def _full_url(self):
-        uurl = build_url(self.kind, self.uuid)
-        return uurl
-
-
-class User(Thing):
-    def __init__(self, username, email=None, password=None, kind='user'):
-        assert isinstance(username, unicode)
-        self.username = username
-        if password is not None:
-            assert isinstance(password, unicode)
-            self.password = password
-        else:
-            self.password = ""
-        if email is not None:
-            assert isinstance(email, unicode)
-            self.email = email
-        else:
-            self.email = ""
-        self.kind = kind
-        self.slug = slugfy(username)
-        self.creation = str(datetime.datetime.now())
-        self.uuid = str(ui.uuid1())
-        m = hashlib.sha1()
-        self.shapassword = hash_it(self.username, self.password)
-        self.attrs = {
-                'username' : self.username,
-                'slug' : self.slug,
-                'email' : self.email,
-                'creation' : self.creation,
-                'uuid' : self.uuid,
-                'shapassword' : self.shapassword,
-                }
-    def _check_credentials(self):
-        if hash_it(self.username, self.password) ==\
-            self.get(self.kind, self.uuid)['shapassword']:
-            return True
-        else:
-            return False
-
-class Photo(Thing):
-    def __init__(self, title_en=None, title_fr=None, kind='photo', artists=None, filetype=None):
-        if isinstance(title_en, unicode):
-            self.title_en = title_en
-        else:
-            self.title_en = u""
-        if isinstance(title_fr, unicode):
-            self.title_fr = title_fr
-        else:
-            self.title_fr = u""
-        self.kind = kind
-        self.slug = slugfy(self.title_en)
-        self.creation = str(datetime.datetime.now())
-        self.uuid = str(ui.uuid1())
-        self.redkey = "%s:%s" % (self.kind, self.uuid)
-        self.description_en = u""
-        self.description_fr = u""
-        self.status_en = ""
-        self.status_fr = ""
-        self.price = None
-        self.artists = []
-	if isinstance(filetype, str):
-            self.filetype = filetype
-            self.path = "%s/raw/%s.%s" % (self.kind, self.slug, self.filetype)
-	else:
-            self.filetype = 'jpg'
-            self.path = "%s/raw/%s.%s" % (self.kind, self.slug, self.filetype)
-        if isinstance(artists, list):
-            for artist in artists:
-                if isinstance(artist, Artist):
-                    self.artists.append(artist.attrs['uuid'])
-                    R.sadd("%s:%s:artists" % (self.kind, self.uuid), artist)
-        elif isinstance(artists, Artist):
-            self.artists.append(artists.attrs['uuid'])
-            R.sadd("%s:%s:photos" % (self.kind, self.uuid), artists)
-        self.attrs = {
-            'title_en' : self.title_en,
-            'slug' : slugfy(self.title_en),
-            'title_fr' : self.title_fr,
-            'description_en' : self.description_en,
-            'description_fr' : self.description_fr,
-            'creation' : self.creation,
-            'uuid' : self.uuid,
-            'status_en' : self.status_en,
-            'status_fr' : self.status_fr,
-            'artists' : self.artists,
-            'path' : self.path,
-            'price' : self.price
-        }
-
-    def _artists(self):
-        """
-        A simple method for getting all the artists. Returns a list of uuids.
-        """
-        #self.artists = R.smembers("%s:%s:artists" % (self.kind, self.uuid))
-        attrs = Thing.get(self)
-        self.attrs['artists'] = attrs['artists']
-        return self.artists
-
-    def _add_artists(self, artists):
-        assert isinstance(artists, list) or isinstance(artists, str)
-        attrs = Thing.get(self)
-        artists.append(attrs['artists'])
-        self.attrs['artists'] = artists
-        Thing.put(self, self.attrs)
-        return self.artists
-
-    def _artist_urls(self):
-        """
-	A generating function
-        """
-	for artist_uuid in self._artists():
-            yield build_url('artist', artist_uuid)
-
-class Artist(User):
-    def __init__(self, name, kind='artist', photos=None):
-        self.name = name
-        self.slug = slugfy(self.name)
-        self.kind = kind
-        self.creation = str(datetime.datetime.now())
-        self.uuid = str(ui.uuid1())
-        self.bio_en = u""
-        self.bio_fr = u""
-        self.photos = []
-	self.photo_paths = []
-        if isinstance(photos, list):
-            for photo in photos:
-                if isinstance(photo, Photo):
-                    self.photos.append(photo.attrs['uuid'])
-                    self.photo_paths.append(photo.attrs['path'])
-                    R.sadd("%s:%s:photos" % (self.kind, self.uuid), photo)
-                    R.sadd("%s:%s:photo_paths" % (self.kind, self.uuid), photo)
-        elif isinstance(photos, Photo):
-            self.photos.append(photos.attrs['uuid'])
-            self.photo_paths.append(photos.attrs['path'])
-            R.sadd("%s:%s:photos" % (self.kind, self.uuid), photos)
-            R.sadd("%s:%s:photo_paths" % (self.kind, self.uuid), photos)
-        self.attrs = {
-            'name' : self.name,
-            'slug' : self.slug,
-            'bio_en' : self.bio_en,
-            'bio_fr' : self.bio_fr,
-            'creation' : self.creation,
-            'uuid' : self.uuid,
-            'photos' : self.photos,
-            'photo_paths' : self.photo_paths,
-        }
-
-    def _photos(self):
-        """
-        A simple method for getting all the photos. Returns a list of kinds:uuids.
-        """
-        #self.photos = R.hgetall("%s:%s:photos" % (self.kind, self.uuid))
-        attrs = Thing.get(self)
-        self.attrs['photos'] = attrs['photos']
-        return self.photos
-
-    def _add_photos(self, photos):
-        assert isinstance(photos, list)
-        attrs = Thing.get(self)
-        photos.append(attrs['photos'])
-        self.attrs['photos'] = photos
-        Thing.put(self, self.attrs)
-        return self.photos
-
-    def _photo_urls(self):
-        """
-	A generating function
-        """
-	for photo_uuid in self._photos():
-            yield build_url('photo', photo_uuid)
-
-    def _photo_paths(self):
-	attrs = Thing.get(self)
-        yield attrs['photo_paths']
+class AudioPost(Post):
+    audio_path = StringField()
 
