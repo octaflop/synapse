737a46563f0771683c9868bbd7731d7787430bea
Faris <octaflop@gmail.com>
2010-09-05 23:01:08 -0700

    To a more workable point after some prodding

--- strings.py
+++ strings.py
@@ -1,11 +1,9 @@
 # -*- encoding: utf-8 -*-
 # strings.py
 # all of the fiddly bits.
-import redis
 import re
 from settings import SALT
-
-R = redis.Redis()
+import datetime
 import hashlib
 
 def slugfy(text, separator='-'):
@@ -19,6 +17,10 @@ def slugfy(text, separator='-'):
   ret = re.sub("\W", " ", ret)
   ret = re.sub(" +", separator, ret)
   return ret.strip()
+
+def format_datetime(timestamp):
+    """Format a timestamp for display."""
+    return datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d @ %H:%M')
 
 def hash_it(username, password):
     try:
@@ -35,44 +37,4 @@ def hash_it(username, password):
         return ret
     except TypeError:
         return False
-
-def add_uurl(kind, slug, tid, uuid):
-    if not R.sadd("%s:slug" % kind, slug):
-        slug += "_"
-        return add_uurl(kind, slug, tid, uuid)
-    else:
-        R.set("%s:%s" % (kind, slug), tid)
-        R.set("%s:%s" % (kind, uuid), slug)
-        R.set("%s" % uuid, slug)
-        R.set("%s:%s:uuid" % (kind, slug), uuid)
-        R.set("%s:%s:uurl" % (kind, tid), slug)
-        return slug
-
-def slug2tid(kind, slug):
-    tid = R.get("%s:%s" % (kind, slug))
-    return tid
-
-def tid2uurl(kind, tid):
-    url = ""
-    tid = slug2tid(kind, slug)
-    url = R.get("%s:%s:uurl" % (kind, tid))
-    return url
-
-def slug2uurl(kind, slug):
-    tid = slug2tid(kind, slug)
-    uurl = R.get("%s:%s:uurl" % (kind, tid))
-    return uurl
-
-def uuid2uurl(uuid):
-    uurl = R.get("%s" % uuid)
-    return uurl
-
-def uurl2uuid(kind, uurl):
-    uuid = R.get("%s:%s:uuid" % (kind, uurl))
-    return uuid
-
-def build_url(kind, uuid):
-    uurl = uuid2uurl(uuid)
-    url = "%s/%s" % (kind, uurl)
-    return url
 
