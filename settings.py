7f54693c3acac4aaf8f67196227900a3ee174a3a
Faris <octaflop@gmail.com>
2010-09-08 12:34:09 -0700

    Not a great commit

--- settings.py
+++ settings.py
@@ -1,10 +1,11 @@
-from flask import g
+import os
+
 #from passwd import *
 DEV = True
 SECRET_KEY = 'vba937ei38mq2'
 SALT = 'vba937ei38mq2'
 #UPLOAD_FOLDER = '/tmp/uploads/'
-UPLOAD_FOLDER = '/home/faris/workspace/projects/aliendog/static/photos/'
+UPLOAD_FOLDER = '%s/static/photos/' % os.getcwd()
 STATIC_PATH = '/static/photos/'
 g = {}
 g['user'] = None
