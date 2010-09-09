19654bc6944505f700c00af6b7f5ed757e661af9
Faris <octaflop@gmail.com>
2010-09-08 17:12:06 -0700

    jQuery UI added. initial site metadata now on the db

--- settings.py
+++ settings.py
@@ -1,4 +1,15 @@
 import os
+from flask import app
+from forms import LoginForm
+
+"""
+NOTE
+For full settings ups: be sure to set up the "site" document.
+
+    >>>> from models import Site
+    >>>> site = Site(title=u'<site title>', motto=u'<site motto>', logo=u'<logo>',\
+    >>>> domain=u'<site domain>')
+"""
 
 #from passwd import *
 DEV = True
@@ -7,8 +18,7 @@ SALT = 'vba937ei38mq2'
 #UPLOAD_FOLDER = '/tmp/uploads/'
 UPLOAD_FOLDER = '%s/static/photos/' % os.getcwd()
 STATIC_PATH = '/static/photos/'
-g = {}
-g['user'] = None
+USER = None
 #ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
