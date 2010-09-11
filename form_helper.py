04563afdb6e3711d2f62ed691e22ba3ac2b45c71
Faris <octaflop@gmail.com>
2010-09-11 16:33:37 -0700

    Finished primary tag field.

---
+++ form_helper.py
@@ -0,0 +1,18 @@
+from flaskext.wtf import TextInput
+from flaskext.wtf import FormField as Field
+
+class TagListField(Field):
+    widget = TextInput()
+
+    def _value(self):
+        if self.data:
+            return u', '.join(self.data)
+        else:
+            return u''
+
+    def process_formdata(self, valuelist):
+        if valuelist:
+            self.data = [x.strip() for x in valuelist[0].split(',')]
+        else:
+            self.data = []
+
