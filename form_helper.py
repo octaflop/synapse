e6056066241d8d56246c4bcee3287e9e0dadb9b4
Faris <octaflop@gmail.com>
2010-09-24 19:00:12 -0700

    Added a cleft. need to add the admin for it, too

--- form_helper.py
+++ form_helper.py
@@ -1,7 +1,7 @@
 from flaskext.wtf import TextInput
 from flaskext.wtf import FormField as Field
 
-class TagListField(Field):
+class AbstractTagListField(Field):
     widget = TextInput()
 
     def _value(self):
@@ -16,3 +16,21 @@ class TagListField(Field):
         else:
             self.data = []
 
+class TagListField(AbstractTagListField):
+    def __init__(self, label='', validators=None, remove_duplicates=True, **kwargs):
+        super(BetterTagListField, self).__init__(label, validators, **kwargs)
+        self.remove_duplicates = remove_duplicates
+
+    def process_formdata(self, valuelist):
+        super(BetterTagListField, self).process_formdata(valuelist)
+        if self.remove_duplicates:
+            self.data = list(self._remove_duplicates(self.data))
+
+    @classmethod
+    def _remove_duplicates(cls, seq):
+        """Remove duplicates in a case insensitive, but case preserving manner"""
+        d = {}
+        for item in seq:
+            if item.lower() not in d:
+                d[item.lower()] = True
+                yield item
