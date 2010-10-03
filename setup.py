5d0a1e0ef644f3b2963556353a58e91c2f973649
Faris <octaflop@gmail.com>
2010-10-02 23:28:03 -0700

    Added the beginnings of a setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Synapse',
+    verson='0.2',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Flask',
+        'MongoWrapper',
+        'MongoEngine',
+        'Markdown'
+        ]
+    )
