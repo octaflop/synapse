25913b310e074a3a1e102d065ae5671a615589e9
Faris <octaflop@gmail.com>
2010-10-04 17:44:56 -0700

    added tests, added bootstrap

---
+++ bootstrap.py
@@ -0,0 +1,22 @@
+import virtualenv, textwrap
+output = virtualenv.create_bootstrap_script(textwrap.dedent("""
+def after_install(options, home_dir):
+    subprocess.call([join(home_dir, 'bin', 'easy_install'),
+                    '-U', 'yolk'])
+    subprocess.call([join(home_dir, 'bin', 'easy_install'),
+                    '-U', 'flask'])
+    subprocess.call([join(home_dir, 'bin', 'easy_install'),
+                    '-U', 'flask-wtf'])
+    subprocess.call([join(home_dir, 'bin', 'easy_install'),
+                    '-U', 'unidecode'])
+    subprocess.call([join(home_dir, 'bin', 'easy_install'),
+                    '-U', 'pymongo'])
+    subprocess.call([join(home_dir, 'bin', 'easy_install'),
+                    '-U', 'mongoengine'])
+    subprocess.call([join(home_dir, 'bin', 'easy_install'),
+                    '-U', 'gunicorn'])
+    subprocess.call([join(home_dir, 'bin', 'easy_install'),
+                    '-U', 'markdown'])
+"""))
+print output
+
