a2bad6abcc0f98f18eaa70a9ce2cbaae5e235b14
Faris <octaflop@gmail.com>
2010-10-07 00:57:47 -0700

    workers and cleaning up. so damn close to production-ready...

--- run.py
+++ run.py
@@ -1,2 +1,19 @@
-from synapse import app
-app.run(debug=True)
+def prod():
+    import subprocess
+    import os
+    from multiprocessing import Process
+
+    Process(target=subprocess.call, args=(
+        (os.path.join('bin', 'gunicorn'), '-b',\
+        'localhost:8000', 'synapse:app'),
+        )).start()
+
+    Process(target=subprocess.call, args=(
+        (os.path.join('usr', 'sbin', 'nginx'), '-c',\
+        os.path.join('conf', 'gunicorn.conf')),
+        )).start()
+
+if __name__ == "__main__":
+    #from synapse import app
+    #app.run(debug=True)
+    print prod()
